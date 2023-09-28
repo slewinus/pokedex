e2a1edcd4372653f683f9b0682f03bd0ec9cc3e0
slewinus <oscar.robert-besle@epitech.eu>
2023-09-28 15:21:47 +0200

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className="App-topbar">
+            <center>
+                <h1>Pokédex</h1>
+            </center>
+        </div>
+    )
+};
+
+export default Header
