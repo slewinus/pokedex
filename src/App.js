874c6880f3bf9d28e8baf996a945f27fc29cc4cb
oscar <oscar.robert-besle@epitech.eu>
2023-12-16 13:03:21 +0100

    test2

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css"
 import Header from './components/Header'
 import PokeCard from './components/PokeCard'
 
-console.log("Hello world")
+console.log("Hello world");
 
 class App extends Component {
   constructor() {
